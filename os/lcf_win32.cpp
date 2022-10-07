598537d3fc436920d1c8841c2f01dc3f78f6b26f
dev_dwarf <logan.c.forman@gmail.com>
2022-10-07 01:28:58 -0600

    started os layer

---
+++ os/lcf_win32.cpp
@@ -0,0 +1,49 @@
+#include "lcf_win32.h"
+
+str8 win32_LoadEntireFile(Arena *arena, str8 path) {
+    str8 fileString = {0};
+
+    DWORD desired_access = GENERIC_READ;
+    DWORD share_mode = 0;
+    SECURITY_ATTRIBUTES security_attributes = {
+        (DWORD)sizeof(SECURITY_ATTRIBUTES),
+        0,
+        0,
+    };
+    DWORD creation_disposition = OPEN_EXISTING;
+    DWORD flags_and_attributes = 0;
+    HANDLE template_file = 0;
+
+    HANDLE file = CreateFileA(path, desired_access, share_mode, &security_attributes,
+                              creation_disposition, flags_and_attributes, template_file);
+
+    if (file != INVALID_HANDLE_VALUE) {
+        LARGE_INTEGER size_int;
+        if (GetFileSizeEx(file, &size_int) && size_int.QuadPart > 0) {
+            u64 size = size.QuadPart;
+            void *data = Arena_take_array(arena, chr8, size);
+            win32_ReadWholeBlock(file, data, size);
+            fileString.str = data;
+            fileString.len = size;
+        }
+        CloseHandle(file);
+    }
+    return fileString;
+}
+
+internal void win32_ReadWholeBlock(HANDLE file, void* data, u64 data_size) {
+    chr8 *ptr = (chr8*) data;
+    chr8 *opl = ptr + data_len;
+    for (;;) {
+        u64 unread = (u64)(opl-ptr);
+        DWORD bytes_to_read = (DWORD)(ClampTop(unread, u32_MAX));
+        DWORD bytes_read = 0;
+        if (!ReadFile(file, ptr, bytes_to_read, &bytes_read, 0)) {
+            break;
+        }
+        ptr += did_read;
+        if (ptr >= opl) {
+            break;
+        }
+    }
+}
--- os/lcf_win32.h
+++ os/lcf_win32.h
@@ -4,3 +4,20 @@
   Description:
   
 ************************************ **/
+
+#ifndef LCF_WIN32
+#define LCF_WIN32 "1.0.0"
+
+#include "../base/lcf_base.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+/* API */
+str8 win32_LoadEntireFile(Arena *arena, str8 path);
+
+/* Helpers */
+internal void win32_ReadWholeBlock(HANDLE file, void* data, u64 data_size);
+
+#endif
+
