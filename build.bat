275d42a07dfe2c28ed72fe9272d8b311ccda19c1
dev_dwarf <logan.c.forman@gmail.com>
2024-01-30 01:00:12 -0700

    Fuck batch

---
+++ build.bat
@@ -0,0 +1,15 @@
+rem @echo off
+@set LIBS=-I C:\Code\ -I C:\Code\raylib\src
+@set DISABLED= -wd4201 -wd4100 -wd4189 -wd4244 -wd4456 -wd4457 -wd4245
+@set FLAGS= -nologo -FC -GR- -Oi -Zi -W4 %DISABLED% /TC
+
+@IF NOT EXIST build mkdir build
+
+@for %%i in (programs\*.c) do call :build %%i
+@goto :eof
+
+:build
+@pushd build
+cl "..\%1" %LIBS% %FLAGS% -link "C:\Code\raylib\raylib.lib" %DISABLE_CONSOLE%
+@popd
+@goto :eof
