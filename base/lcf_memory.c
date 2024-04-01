43f56cd1c4857d03417304379e8f1ccd61bff70a
dev_dwarf <logan.c.forman@gmail.com>
2024-03-31 20:36:24 -0600

    Correct backwards conditional

--- base/lcf_memory.c
+++ base/lcf_memory.c
@@ -44,7 +44,7 @@ void* Arena_take_custom(Arena *a, u64 size, u32 alignment) {
     /* Check that there is space */
     if (new_pos < a->size) {
         /* Commit memory if needed */
-        b32 in_commit_range = new_pos > a->commit_pos;
+        b32 in_commit_range = new_pos <= a->commit_pos;
         if (!in_commit_range) {
             upr new_commit_pos = next_alignment(mem, a->pos, a->commit_size);
             in_commit_range = LCF_MEMORY_commit(mem, new_commit_pos); 
