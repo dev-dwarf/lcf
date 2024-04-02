7e5e719934e4f6ed701039c16d72cd91e0eacf23
dev_dwarf <logan.c.forman@gmail.com>
2024-04-02 01:39:20 -0600

    Break some thigns out as params

--- win32_build.c
+++ win32_build.c
@@ -1,4 +1,3 @@
-
 #include "lcf/lcf.h"
 #include "lcf/lcf.c"
 
@@ -8,6 +7,10 @@
 // TODO(lcf) bug in win32_GetFileInfo that makes src path wrong in the below
 
 // NOTE(lcf): should be run with "build" as working dir
+
+#define CODE_ROOT "C:\\Code\\"
+#define FLAGS "-GR- -Oi -Zi"
+#define DISABLED_WARNINGS "-wd4201 -wd4100 -wd4189 -wd4244 -wd4456 -wd4457 -wd4245"
 
 int main(void) {
     Arena *a = Arena_scratch();
@@ -19,7 +22,7 @@ int main(void) {
         os_FileInfo exe = os_GetFileInfo(a, exe_file);
 
         if (exe.written < src.written) {
-            str cmd = strf(a, "cl ..\\programs\\%.*s -I C:\\Code\\ -I C:\\Code\\raylib\\src -nologo -GR- -Oi -Zi -W4 -wd4201 -wd4100 -wd4189 -wd4244 -wd4456 -wd4457 -wd4245 -link \"C:\\Code\\raylib\\raylib.lib\"", (s32)src.name.len, src.name.str);
+            str cmd = strf(a, "cl ..\\programs\\%.*s -I %s -I %sraylib\\src -nologo %s -W4 %s -link \"%sraylib\\raylib.lib\"", (s32)src.name.len, src.name.str, CODE_ROOT, CODE_ROOT, FLAGS, DISABLED_WARNINGS, CODE_ROOT);
             printf("\nbuilding programs\\%.*s: \n\t%.*s\n", (s32)src.name.len, src.name.str, (s32)cmd.len, cmd.str);
             fflush(0);
             system(cmd.str);
